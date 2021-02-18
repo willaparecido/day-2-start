f549c75276ad0ec852c2bf428cd755db3dbcdc39
willaparecido <wilson_aparecido2015@outlook.com>
2021-02-18 06:07:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#Data Types
+
+#String
+
+print("Hello"[4])
+print("Wilson"[5])
+
+print("123" + "456")   #it's no considered like number ,is one string.
+
+#integer
+
+print(123 + 456)
+
+print(123_456_789)
+
+#float   ( floating pint number)
+
+3.14159
+
+#Boolean 
+
+True 
+False
